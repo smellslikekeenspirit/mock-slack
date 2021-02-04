8840878d5f7fd9ed32c0cd647c6b88acb8c6415c
pdn3628 <pdn3628@g.rit.edu>
2021-02-03 23:12:43 -0500

    adding idempotent test to test_chat.py

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -8,8 +8,18 @@ class TestChat(unittest.TestCase):
         """Build the tables"""
         conn = connect()
         cur = conn.cursor()
-        buildTables()
+        rebuildTables()
         cur.execute('SELECT * FROM example_table')
         self.assertEqual([], cur.fetchall(), "no rows in example_table")
         conn.close()
+
+    def test_rebuild_tables_is_idempotent(self):
+      """Drop and rebuild the tables twice"""
+      rebuildTables()
+      rebuildTables()
+      conn = connect()
+      cur = conn.cursor()
+      cur.execute('SELECT * FROM example_table')
+      self.assertEqual([], cur.fetchall(), "no rows in example_table")
+      conn.close()
         
