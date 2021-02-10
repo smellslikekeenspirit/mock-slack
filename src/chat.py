2425ab95e43c42ed0396883dc1a41a517800eedc
smellslikekeenspirit <pdn3628@rit.edu>
2021-02-10 09:42:10 -0500

    modified my tests according to test sketches

--- src/chat.py
+++ src/chat.py
@@ -20,14 +20,17 @@ def rebuild_tables():
             user_id              SERIAL PRIMARY KEY NOT NULL,
             name                 VARCHAR(30) NOT NULL,
             phone_number         VARCHAR(10) NOT NULL,
-            email                VARCHAR(40) NOT NULL
+            email                VARCHAR(40) NOT NULL,
+            suspended_since      TIMESTAMP,
+            suspended_till       TIMESTAMP
         );
         CREATE TABLE messages(
             message_id          SERIAL PRIMARY KEY NOT NULL,
-            sender_user_id      SERIAL NOT NULL,
-            receiver_user_id    SERIAL NOT NULL,
+            sender_id           SERIAL NOT NULL,
+            receiver_id         SERIAL NOT NULL,
+            time_sent           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             message             TEXT NOT NULL,
-            time_sent           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            is_read             BOOLEAN DEFAULT FALSE
         );
     """
     cur.execute(drop_example)
@@ -49,8 +52,8 @@ def create_user(user_id: int, name: str, phone_number: int, email: str) -> None:
     """
     conn = connect()
     cur = conn.cursor()
-    string = f'INSERT INTO users(user_id, name, phone_number, email) VALUES ({user_id},\'{name}\',' \
-             f'{phone_number}, \'{email}\')'
+    string = f'INSERT INTO users(user_id, name, phone_number, email, suspended_since, suspended_till)' \
+             f'VALUES ({user_id},\'{name}\', {phone_number}, \'{email}\')'
     print(string)
     cur.execute(string)
 
@@ -67,6 +70,35 @@ def create_message(message_id: int, sender_user_id: int, receiver_user_id: int, 
     conn = connect()
     cur = conn.cursor()
     dt = datetime.now()
-    cur.execute(f'INSERT INTO users(message_id, sender_user_id, receiver_user_id, message, time_sent)'
+    cur.execute(f'INSERT INTO users(message_id, sender_id, receiver_id, time_sent, message, is_read)'
                 f'VALUES (\'{message_id}\', \'{sender_user_id}\', '
                 f'\'{receiver_user_id}\', \'{message}\', \'{dt.now()}\')')
+
+
+def populate_tables():
+    conn = connect()
+    cur = conn.cursor()
+    # check why this is necessary in CI
+    rebuild_tables()
+    add_users = """
+                    INSERT INTO users(user_id, name, phone_number, email, suspended_since, suspended_till) VALUES
+                        (1, 'Abbott', '1234567890', 'abbott@rit.edu', NULL, NULL),
+                        (2, 'Costello', '1234567891', 'costello@rit.edu', NULL, NULL),
+                        (3, 'Moe', '1234567892', 'moe@rit.edu', NULL, NULL),
+                        (4, 'Larry', '1234567893', 'larry@rit.edu', '1990-01-01 00:00:00', '2060-01-01 00:00:00'),
+                        (5, 'Curly', '1234567894', 'curly@rit.edu', '1990-01-01 00:00:00', '2000-01-01 00:00:00');
+                """
+    cur.execute(add_users)
+    add_messages = """
+                    INSERT INTO messages(message_id, sender_id, receiver_id, time_sent, message, is_read) VALUES
+                        (1, 1, 2, '2000-02-12 11:00:00', 'C! How are you?', TRUE),
+                        (2, 2, 1, '2000-02-12 12:00:40', 'Hey A! Fine. You?', TRUE),
+                        (3, 3, 4, '1995-02-12 11:00:00', 'How are you Larry?', TRUE),
+                        (4, 4, 3, '1995-02-12 12:00:40', 'Fine, Moe! You?', TRUE),
+                        (5, 3, 4, '1922-02-12 12:00:40', 'I got fired.', FALSE),
+                        (6, 2, 1, '2020-02-12 11:00:00', 'Abbott! So long. How are you?', FALSE),
+                        (7, 3, 1, '2020-02-12 11:10:00', 'Abbott, this is Moe. Hi!', FALSE);  
+                """
+    cur.execute(add_messages)
+    conn.commit()
+    conn.close()
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -24,48 +24,89 @@ class TestChat(unittest.TestCase):
         self.assertEqual([], cur.fetchall(), "no rows in users")
         conn.close()
 
-    def test_add_users(self):
+    # database is seeded with a test data set without crashing
+    def test_populate_tables(self):
+        print("Test that database is seeded with data without crashing")
         conn = connect()
         cur = conn.cursor()
         # check why this is necessary in CI
-        rebuild_tables()
-        add_users = """
-                INSERT INTO users(user_id, name, phone_number, email) VALUES
-                    (1, 'Prionti', 5855689678, 'pdn3628@rit.edu'),
-                    (2, 'John', 5855689679, 'jp2345@rit.edu');
-            """
-        cur.execute(add_users)
-        # create_user(1, 'Prionti', 5855689678, 'pdn3628@rit.edu')
-        # create_user(2, 'John', 5855689679, 'jp2345@rit.edu')
+        populate_tables()
         cur.execute('SELECT * FROM users')
-        self.assertEqual(2, len(cur.fetchall()), "two rows in users table")
-
-    def test_add_messages(self):
-        conn = connect()
-        cur = conn.cursor()
-        rebuild_tables()
-        add_messages = """
-                INSERT INTO messages(message_id, sender_user_id, receiver_user_id, message, time_sent) VALUES
-                    (1, 1, 2, 'How are you?', '2021-02-12 11:00:00'),
-                    (2, 2, 1, 'Fine! You?', '2021-02-12 12:00:40');
-            """
-        cur.execute(add_messages)
-        # create_user(1, 'Prionti', 5855689678, 'pdn3628@rit.edu')
-        # create_user(2, 'John', 5855689679, 'jp2345@rit.edu')
+        self.assertEqual(5, len(cur.fetchall()), "There should be 5 rows in users table")
         cur.execute('SELECT * FROM messages')
-        self.assertEqual(2, len(cur.fetchall()), "two rows in messages table")
+        self.assertEqual(7, len(cur.fetchall()), "There should be 7 rows in messages table")
 
-    def test_get_by_phone_number(self):
+    # we can get all messages sent by a specific user
+    def test_messages_sent_by(self):
+        print("Test the messages sent by a user given a timeframe")
         conn = connect()
         cur = conn.cursor()
-        add_users = """
-                        INSERT INTO users(user_id, name, phone_number, email) VALUES
-                            (1, 'Prionti', 5855689678, 'pdn3628@rit.edu'),
-                            (2, 'John', 5855689679, 'jp2345@rit.edu');
-                    """
-        cur.execute(add_users)
-        cur.execute('SELECT * FROM users WHERE phone_number = \'5855689679\'')
-        user = cur.fetchall()
-        self.assertTrue('John' == user[0][1], "5855689679 is John's phone number")
+        populate_tables()
+        cur.execute(
+            'SELECT messages.message_id FROM messages INNER JOIN users ON users.user_id = messages.sender_id WHERE '
+            'EXTRACT( '
+            'year FROM messages.time_sent) > 1990 AND users.name = \'Costello\'')
+        message_id_list = cur.fetchall()
+        self.assertTrue(2 == len(message_id_list), "Costello sent 2 messages after 1990")
 
+    # we can get all the messages between Abbott and Costello for all time
+    # We can get all the messages between Moe and Larry during the year 1995
+    def test_all_messages_between(self):
+        print("Test retrieval of all messages between two users")
+        conn = connect()
+        cur = conn.cursor()
+        populate_tables()
+        cur.execute(
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 1 '
+            'AND messages.receiver_id = 2')
+        a_to_c = len(cur.fetchall())
+        cur.execute(
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 2 '
+            'AND messages.receiver_id = 1')
+        c_to_a = len(cur.fetchall())
+        self.assertTrue(3 == a_to_c + c_to_a, "Abbott and Costello exchanged 3 messages")
+        print("Test retrieval of all messages between two users given a timeframe")
+        cur.execute(
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 3 '
+            'AND messages.receiver_id = 4 AND EXTRACT(year FROM time_sent) = 1995')
+        m_to_l = len(cur.fetchall())
+        cur.execute(
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 4 '
+            'AND messages.receiver_id = 3 AND EXTRACT(year FROM time_sent) = 1995')
+        l_to_m = len(cur.fetchall())
+        self.assertTrue(2 == m_to_l + l_to_m, "Abbott and Costello exchanged 2 messages in 1995")
 
+    # number of unread messages to Abbott is correct
+    def test_unread_messages(self):
+        print("Test keeping track of read/unread messages")
+        conn = connect()
+        cur = conn.cursor()
+        populate_tables()
+        cur.execute(
+            'SELECT messages.message_id FROM messages WHERE messages.is_read = TRUE '
+            'AND messages.receiver_id = 1')
+        reads = len(cur.fetchall())
+        cur.execute(
+            'SELECT messages.message_id FROM messages WHERE messages.is_read = FALSE '
+            'AND messages.receiver_id = 1')
+        unreads = len(cur.fetchall())
+        self.assertTrue(1 == reads, "Abbott has 1 read message")
+        self.assertTrue(2 == unreads, "Abbott has 2 unread messages")
+
+    # If today is May 4, 2012, then Larry is suspended
+    # If today is February 29, 2000, Curly is not suspended
+    def test_suspensions(self):
+        """Test suspensions are correct"""
+        print("Test that suspensions are in place correctly")
+        conn = connect()
+        cur = conn.cursor()
+        populate_tables()
+        larry_date = datetime(2012, 5, 4)
+        curly_date = datetime(2000, 2, 29)
+        cur.execute('SELECT suspended_since, suspended_till FROM users WHERE name = \'Larry\'')
+        suspension_date = cur.fetchall()
+        self.assertTrue((suspension_date[0][0] < larry_date) and (larry_date < suspension_date[0][1]),
+                        "Larry should stay suspended on May 4 2012")
+        cur.execute('SELECT suspended_till FROM users WHERE name = \'Curly\'')
+        self.assertTrue(suspension_date[0][0] < curly_date, "Curly should not be suspended on February 29 2000")
+        conn.close()
