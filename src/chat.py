845a68d01938dfc42517d5d4fa9aeff5c94cf269
smellslikekeenspirit <pdn3628@rit.edu>
2021-02-17 23:53:56 -0500

    added db2 tests to chat

--- src/chat.py
+++ src/chat.py
@@ -1,3 +1,4 @@
+import csv
 from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
@@ -20,17 +21,20 @@ def rebuild_tables():
     """
     create_schema = """
         CREATE TABLE users(
-            user_id              SERIAL PRIMARY KEY NOT NULL,
+            user_id              VARCHAR(30) PRIMARY KEY NOT NULL,
             name                 VARCHAR(30) NOT NULL,
             phone_number         VARCHAR(10) NOT NULL,
             email                VARCHAR(40) NOT NULL,
+            userid_set           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            userid_reset         TIMESTAMP DEFAULT NULL,
             suspended_since      TIMESTAMP,
-            suspended_till       TIMESTAMP
+            suspended_till       TIMESTAMP,
+            UNIQUE(email)
         );
         CREATE TABLE messages(
             message_id          SERIAL PRIMARY KEY NOT NULL,
-            sender_id           SERIAL NOT NULL,
-            receiver_id         SERIAL NOT NULL,
+            sender_id           VARCHAR(30) NOT NULL,
+            receiver_id         VARCHAR(30) NOT NULL,
             time_sent           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             message             TEXT NOT NULL,
             is_read             BOOLEAN DEFAULT FALSE
@@ -44,25 +48,53 @@ def rebuild_tables():
     conn.close()
 
 
+def rebuild_messages():
+    """
+    rebuild new messages table
+    """
+    conn = connect()
+    cur = conn.cursor()
+    drop_messages = """
+        DROP TABLE IF EXISTS messages
+    """
+    create_messages = """
+        CREATE TABLE messages(
+            message_id          SERIAL PRIMARY KEY NOT NULL,
+            sender_id           VARCHAR(30) NOT NULL,
+            receiver_id         VARCHAR(30) NOT NULL,
+            time_sent           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            message             TEXT NOT NULL,
+            is_read             BOOLEAN DEFAULT FALSE
+        )
+    """
+
+    cur.execute(drop_messages)
+    cur.execute(create_messages)
+    conn.commit()
+    conn.close()
+
+
 def user_exists(email):
     """
 
     :param email:
     :return:
     """
-    matches = exec_get_all('SELECT email FROM users WHERE email = %s', email)
+    matches = exec_get_all('SELECT email FROM users WHERE email = %s', (email,))
     return len(matches) == 1
 
 
 def get_email_by_id(user_id):
-    matches = exec_get_all('SELECT email FROM users WHERE user_id = %s', user_id)
+    matches = exec_get_all('SELECT email FROM users WHERE user_id = %s', (user_id,))
     return matches[0]
 
 
-def create_user(user_id: int, name: str, phone_number: int, email: str,
-                suspended_since: None, suspended_till: None) -> str:
+def create_user(user_id: str, name: str, phone_number: int, email: str, userid_set: datetime,
+                userid_reset: None, suspended_since: None, suspended_till: None) -> str:
     """
-    Creates a user
+    Creates a new user
+    :param userid_set:
+    :param userid_reset:
     :param user_id: unique id of user
     :param name: name of user
     :param email: user's email address
@@ -77,19 +109,21 @@ def create_user(user_id: int, name: str, phone_number: int, email: str,
     if user_exists(email):
         return "User already exists"
 
-    if len(name) < 6 or len(name) > 30:
+    if len(user_id) < 6 or len(user_id) > 30:
         return "Username needs to be between 8 to 30 characters"
-
-    string = f'INSERT INTO users(user_id, name, phone_number, email, suspended_since, suspended_till)' \
-             f'VALUES ({user_id},\'{name}\', {phone_number},' \
-             f'\'{email}\', {suspended_since}, {suspended_till})'
+    if not userid_set:
+        userid_set = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    string = f'INSERT INTO users(user_id, name, phone_number, email, userid_set, ' \
+             f'userid_reset, suspended_since, suspended_till)' \
+             f'VALUES (\'{user_id}\',\'{name}\', {phone_number},' \
+             f'\'{email}\', \'{userid_set}\', NULL, NULL, NULL)'
     exec_commit(string)
     return "User added successfully"
 
 
 def change_username(user_id, new_name, change_time=None):
     """
-    changes username if it has not been changed in the last six months
+    changes username (user_id in db) if it has not been changed in the last six months
     :param user_id:
     :param new_name:
     :param change_time:
@@ -107,19 +141,19 @@ def change_username(user_id, new_name, change_time=None):
     if change_time:
         new_time = datetime.strptime(change_time, '%Y-%m-%d %H:%M:%S')
 
-    change_date_list = exec_get_all('SELECT username_change_date FROM users WHERE email = %s', (email,))
+    change_date_list = exec_get_all('SELECT userid_reset FROM users WHERE email = %s', (email,))
     change_date = change_date_list[0][0]
     six_months_later = relativedelta(months=6)
 
-    if not change_date or change_date + six_months_later <= new_time:
-        exec_commit('UPDATE users SET username_change_date = %s WHERE email = %s', (new_time, email))
-        exec_commit('UPDATE users SET username = %s WHERE email = %s', (new_name, email))
+    if not change_date or (change_date + six_months_later <= new_time):
+        exec_commit('UPDATE users SET userid_reset = %s WHERE email = %s', (new_time, email))
+        exec_commit('UPDATE users SET user_id = %s WHERE email = %s', (new_name, email))
         return "User successfully changed username to " + new_name
     return "User changed their username in the last 6 months"
 
 
-def create_message(message_id: int, sender_id: int,
-                   receiver_id: int, time_sent: None, message: str) -> str:
+def create_message(message_id: int, sender_id: str,
+                   receiver_id: str, time_sent: None, message: str) -> str:
     """
     adds a message to db
     :param message_id: message id
@@ -136,10 +170,11 @@ def create_message(message_id: int, sender_id: int,
     if not user_exists(receiver_email):
         return "User" + receiver_email + "doesn't exist"
     init_time = datetime.now()
+    init_time = str(init_time)
     if time_sent:
         init_time = datetime.strptime(time_sent, '%Y-%m-%d %H:%M:%S')
 
-    suspension_dates = exec_get_all('SELECT suspended_start, suspended_end FROM users WHERE email = %s',
+    suspension_dates = exec_get_all('SELECT suspended_since, suspended_till FROM users WHERE email = %s',
                                     (sender_email,))
     suspended_start = suspension_dates[0][0]
     suspended_end = suspension_dates[0][1]
@@ -148,9 +183,9 @@ def create_message(message_id: int, sender_id: int,
             return sender_email + " is currently suspended until " + suspended_end.strftime("%Y/%m/%d %H:%M:%S")
 
     # If no time was given it will default to the current time
-    exec_commit(f'INSERT INTO users(message_id, sender_id, receiver_id, time_sent, message, is_read)'
-                f' VALUES (%s,%s,%s,%s)',
-                (message_id, sender_id, receiver_id, init_time, message, False))
+    exec_commit(f'INSERT INTO messages(message_id, sender_id, receiver_id, time_sent, message)'
+                f' VALUES (%s,%s,%s,%s,%s)',
+                (message_id, sender_id, receiver_id, init_time, message))
     return "Message sent successfully"
 
 
@@ -161,10 +196,10 @@ def read_message(message_id, receiver_id):
     :param receiver_id:
     :return: text content
     """
-    texts = exec_get_all('SELECT text FROM messages WHERE message_id = %s AND receiver_id = %s',
+    texts = exec_get_all('SELECT message FROM messages WHERE message_id = %s AND receiver_id = %s',
                              (message_id, receiver_id))
     if len(texts) == 1:
-        exec_commit('UPDATE messages SET is_read = TRUE WHERE id = %s', (message_id,))
+        exec_commit('UPDATE messages SET is_read = TRUE WHERE message_id = %s', (message_id,))
         return texts[0][0]
 
 
@@ -176,8 +211,7 @@ def get_unread_messages(receiver_id):
     """
     if not user_exists(get_email_by_id(receiver_id)):
         return []
-    unreads = exec_get_all('SELECT id FROM messages WHERE is_read = FALSE'
-                           'AND receiver_id = %s', (receiver_id,))
+    unreads = exec_get_all('SELECT * FROM messages WHERE is_read = FALSE AND receiver_id = %s', (receiver_id,))
     return unreads
 
 
@@ -194,7 +228,7 @@ def get_messages_from(receiver_id, sender_id):
         return "User" + sender_email + "doesn't exist"
     if not user_exists(receiver_email):
         return "User" + receiver_email + "doesn't exist"
-    return exec_get_all('SELECT id FROM messages WHERE sender_id = %s AND receiver_id = %s',
+    return exec_get_all('SELECT message FROM messages WHERE sender_id = %s AND receiver_id = %s',
                         (sender_id, receiver_id))
 
 
@@ -208,7 +242,7 @@ def suspend_user(email, end_suspension, start_suspension=None):
     """
     if not start_suspension:
         start_suspension = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    exec_commit('UPDATE users SET suspended_start = %s AND suspended_end = %s WHERE email = %s',
+    exec_commit('UPDATE users SET suspended_since = %s AND suspended_till = %s WHERE email = %s',
                 (start_suspension, end_suspension, email))
     return email + " is suspended from " + start_suspension + " until " + end_suspension
 
@@ -219,34 +253,81 @@ def resume_user(email):
     :param email:
     :return:
     """
-    exec_commit('UPDATE users SET suspended_start = NULL AND suspended_end = NULL WHERE email = %s', email)
+    exec_commit('UPDATE users SET suspended_since = NULL AND suspended_till = NULL WHERE email = %s', email)
     return email + " is no longer suspended"
 
 
-def populate_tables():
+def get_last_message_id():
+    msg_id = exec_commit('SELECT message_id FROM messages ORDER BY message_id DESC LIMIT 1')
+    return msg_id
+
+
+def read_csv(filename):
+    """
+    Reads in the who's on first csv file
+
+    Keyword arguments:
+    filename -- the whos on first csv files name (Default whos_on_first.csv)
+
+    Return:
+    None
+    """
+    with open(filename) as file:
+        reader = csv.reader(file, delimiter=",")
+        next(reader, None)
+        msg_id = get_last_message_id()
+        if not msg_id:
+            msg_id = 1
+        else:
+            msg_id += 1
+        for row in reader:
+            if row[0] == 'Abbott':
+                create_message(msg_id, 'Abbott1234', 'Costello1234', None, row[1])
+            else:
+                create_message(msg_id, 'Costello1234', 'Abbott1234', None, row[1])
+            msg_id += 1
+
+
+def populate_tables_db1():
     conn = connect()
     cur = conn.cursor()
     # check why this is necessary in CI
     rebuild_tables()
     add_users = """
-                    INSERT INTO users(user_id, name, phone_number, email, suspended_since, suspended_till) VALUES
-                        (1, 'Abbott', '1234567890', 'abbott@rit.edu', NULL, NULL),
-                        (2, 'Costello', '1234567891', 'costello@rit.edu', NULL, NULL),
-                        (3, 'Moe', '1234567892', 'moe@rit.edu', NULL, NULL),
-                        (4, 'Larry', '1234567893', 'larry@rit.edu', '1990-01-01 00:00:00', '2060-01-01 00:00:00'),
-                        (5, 'Curly', '1234567894', 'curly@rit.edu', '1990-01-01 00:00:00', '2000-01-01 00:00:00');
+                    INSERT INTO users(user_id, name, phone_number, email,
+                    userid_set, userid_reset, suspended_since, suspended_till) VALUES
+                        ('Abbott1234', 'Abbott', '1234567890', 'abbott@rit.edu', '1990-01-01 00:00:00',
+                         NULL, NULL, NULL),
+                        ('Costello1234', 'Costello', '1234567891', 'costello@rit.edu', '1990-01-01 00:00:00',
+                         NULL, NULL, NULL),
+                        ('Moe1234', 'Moe', '1234567892', 'moe@rit.edu', '1990-01-01 00:00:00', NULL, NULL, NULL),
+                        ('Larry1234', 'Larry', '1234567893', 'larry@rit.edu', 
+                        '1989-01-01 00:00:00', NULL, '1990-01-01 00:00:00', '2060-01-01 00:00:00'),
+                        ('Curly1234', 'Curly', '1234567894', 'curly@rit.edu', 
+                        '1989-01-01 00:00:00', NULL, '1990-01-01 00:00:00', '2000-01-01 00:00:00');
                 """
     cur.execute(add_users)
     add_messages = """
                     INSERT INTO messages(message_id, sender_id, receiver_id, time_sent, message, is_read) VALUES
-                        (1, 1, 2, '2000-02-12 11:00:00', 'C! How are you?', TRUE),
-                        (2, 2, 1, '2000-02-12 12:00:40', 'Hey A! Fine. You?', TRUE),
-                        (3, 3, 4, '1995-02-12 11:00:00', 'How are you Larry?', TRUE),
-                        (4, 4, 3, '1995-02-12 12:00:40', 'Fine, Moe! You?', TRUE),
-                        (5, 3, 4, '1922-02-12 12:00:40', 'I got fired.', FALSE),
-                        (6, 2, 1, '2020-02-12 11:00:00', 'Abbott! So long. How are you?', FALSE),
-                        (7, 3, 1, '2020-02-12 11:10:00', 'Abbott, this is Moe. Hi!', FALSE);  
+                        (1, 'Abbott1234', 'Costello1234', '2000-02-12 11:00:00', 'C! How are you?', TRUE),
+                        (2, 'Costello1234', 'Abbott1234', '2000-02-12 12:00:40', 'Hey A! Fine. You?', TRUE),
+                        (3, 'Moe1234', 'Larry1234', '1995-02-12 11:00:00', 'How are you Larry?', TRUE),
+                        (4, 'Larry1234', 'Moe1234', '1995-02-12 12:00:40', 'Fine, Moe! You?', TRUE),
+                        (5, 'Moe1234', 'Larry1234', '1922-02-12 12:00:40', 'I got fired.', FALSE),
+                        (6, 'Costello1234', 'Abbott1234', '2020-02-12 11:00:00',
+                        'Abbott! So long. How are you?', FALSE),
+                        (7, 'Moe1234', 'Abbott1234', '2020-02-12 11:10:00', 'Abbott, this is Moe. Hi!', FALSE);  
                 """
     cur.execute(add_messages)
     conn.commit()
     conn.close()
+
+
+def populate_tables_db2():
+    rebuild_tables()
+    create_user('DrMarvin', 'Marvin', 5855556656, 'drmarvin@rit.edu', '1991-05-16 00:00:00', None, None, None)
+    create_user('Bob12345', 'Bob', 5855654534, 'bob@rit.edu', '1991-05-17 00:00:00', None, None, None)
+    create_message(1, 'Bob12345', 'DrMarvin', '1991-05-18 00:00:00', 'I\'m doing the work, I\'m baby-stepping')
+    change_username('Bob12345', 'BabySteps2Door', '1991-05-19 00:00:00')
+    change_username('BabySteps2Door', 'BabySteps2Elevator', '1991-05-20 00:00:00')
+
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -30,7 +30,7 @@ class TestChat(unittest.TestCase):
         conn = connect()
         cur = conn.cursor()
         # check why this is necessary in CI
-        populate_tables()
+        populate_tables_db1()
         cur.execute('SELECT * FROM users')
         self.assertEqual(5, len(cur.fetchall()), "There should be 5 rows in users table")
         cur.execute('SELECT * FROM messages')
@@ -41,7 +41,7 @@ class TestChat(unittest.TestCase):
         print("Test the messages sent by a user given a timeframe")
         conn = connect()
         cur = conn.cursor()
-        populate_tables()
+        populate_tables_db1()
         cur.execute(
             'SELECT messages.message_id FROM messages INNER JOIN users ON users.user_id = messages.sender_id WHERE '
             'EXTRACT( '
@@ -55,40 +55,40 @@ class TestChat(unittest.TestCase):
         print("Test retrieval of all messages between two users")
         conn = connect()
         cur = conn.cursor()
-        populate_tables()
+        populate_tables_db1()
         cur.execute(
-            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 1 '
-            'AND messages.receiver_id = 2')
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = \'Abbott1234\' '
+            'AND messages.receiver_id = \'Costello1234\'')
         a_to_c = len(cur.fetchall())
         cur.execute(
-            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 2 '
-            'AND messages.receiver_id = 1')
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = \'Costello1234\' '
+            'AND messages.receiver_id = \'Abbott1234\'')
         c_to_a = len(cur.fetchall())
         self.assertTrue(3 == a_to_c + c_to_a, "Abbott and Costello exchanged 3 messages")
         print("Test retrieval of all messages between two users given a timeframe")
         cur.execute(
-            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 3 '
-            'AND messages.receiver_id = 4 AND EXTRACT(year FROM time_sent) = 1995')
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = \'Moe1234\' '
+            'AND messages.receiver_id = \'Larry1234\' AND EXTRACT(year FROM time_sent) = 1995')
         m_to_l = len(cur.fetchall())
         cur.execute(
-            'SELECT messages.message_id FROM messages WHERE messages.sender_id = 4 '
-            'AND messages.receiver_id = 3 AND EXTRACT(year FROM time_sent) = 1995')
+            'SELECT messages.message_id FROM messages WHERE messages.sender_id = \'Larry1234\' '
+            'AND messages.receiver_id = \'Moe1234\' AND EXTRACT(year FROM time_sent) = 1995')
         l_to_m = len(cur.fetchall())
-        self.assertTrue(2 == m_to_l + l_to_m, "Abbott and Costello exchanged 2 messages in 1995")
+        self.assertTrue(2 == m_to_l + l_to_m, "Moe and Larry exchanged 2 messages in 1995")
 
     # number of unread messages to Abbott is correct
     def test_unread_messages(self):
         print("Test keeping track of read/unread messages")
         conn = connect()
         cur = conn.cursor()
-        populate_tables()
+        populate_tables_db1()
         cur.execute(
             'SELECT messages.message_id FROM messages WHERE messages.is_read = TRUE '
-            'AND messages.receiver_id = 1')
+            'AND messages.receiver_id = \'Abbott1234\'')
         reads = len(cur.fetchall())
         cur.execute(
             'SELECT messages.message_id FROM messages WHERE messages.is_read = FALSE '
-            'AND messages.receiver_id = 1')
+            'AND messages.receiver_id = \'Abbott1234\'')
         unreads = len(cur.fetchall())
         self.assertTrue(1 == reads, "Abbott has 1 read message")
         self.assertTrue(2 == unreads, "Abbott has 2 unread messages")
@@ -100,7 +100,7 @@ class TestChat(unittest.TestCase):
         print("Test that suspensions are in place correctly")
         conn = connect()
         cur = conn.cursor()
-        populate_tables()
+        populate_tables_db1()
         larry_date = datetime(2012, 5, 4)
         curly_date = datetime(2000, 2, 29)
         cur.execute('SELECT suspended_since, suspended_till FROM users WHERE name = \'Larry\'')
@@ -110,3 +110,31 @@ class TestChat(unittest.TestCase):
         cur.execute('SELECT suspended_till FROM users WHERE name = \'Curly\'')
         self.assertTrue(suspension_date[0][0] < curly_date, "Curly should not be suspended on February 29 2000")
         conn.close()
+
+    def test_whos_on_first(self):
+        print("Test parsing of CSV file")
+        populate_tables_db1()
+        rebuild_messages()
+        read_csv('../data/whos_on_first.csv')
+        all_unread_messages = exec_get_all('SELECT message_id FROM messages WHERE is_read = FALSE')
+        self.assertTrue(len(all_unread_messages) == 184, "184 unread messages should have been read in from the csv")
+
+    def test_users_added(self):
+        print("Test that users are added correctly from db2 test data")
+        populate_tables_db2()
+        username_list = exec_get_all('SELECT user_id FROM users')
+        expected_user_list = ['DrMarvin', 'BabySteps2Door']
+        self.assertTrue(len(username_list) == len(expected_user_list), "Not all users were added")
+        for user in username_list:
+            username = user[0]
+            self.assertTrue(username in expected_user_list, username + " shouldn't be in the users table")
+
+    def test_checking_messages(self):
+        print("Test that messages are sent correctly from db2 test data")
+        populate_tables_db2()
+        message_list = get_unread_messages('DrMarvin')
+        self.assertTrue(len(message_list) == 1, "DrMarvin should have 1 unread message")
+        message_id = message_list[0][0]
+        read_message(message_id, 'DrMarvin')
+        message_list = get_unread_messages('DrMarvin')
+        self.assertTrue(len(message_list) == 0, "DrMarvin should have read his unread message")
